d2649bd2dd69b72a3380a3ddc0eecabc4bce7d18
pat-s <patrick.schratz@gmail.com>
2020-07-30 10:10:00 +0200

    lintr

--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,7 @@ register_tasks = function() {
   x$add("kc_housing", get_kc_housing_task())
 }
 
-.onLoad = function(libname, pkgname) {
+.onLoad = function(libname, pkgname) { # nolint
   # nocov start
   if (isNamespaceLoaded("mlr3")) {
     register_tasks()
@@ -17,7 +17,7 @@ register_tasks = function() {
 } # nocov end
 
 
-.onUnload = function(libpath) {
+.onUnload = function(libpath) { # nolint
   # nocov start
   event = packageEvent("mlr3", "onLoad")
   hooks = getHook(event)
