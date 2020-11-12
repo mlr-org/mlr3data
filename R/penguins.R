b5910ccdcf7f27aeb9325a58c12f7ce2c925115f
Michel Lang <michellang@gmail.com>
2020-11-12 09:34:38 +0100

    improve docs

--- R/penguins.R
+++ R/penguins.R
@@ -15,7 +15,7 @@
 #' @source \CRANpkg{palmerpenguins}
 #'
 #' @references
-#' `r tools::toRd(bibentries["gorman2014"])`
+#' `r format_bib("gorman2014")`
 #'
 #' \url{https://github.com/allisonhorst/palmerpenguins}
 #'
