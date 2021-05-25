080960cb5de43d04cd48a7a5b786736034ded025
Michel Lang <michellang@gmail.com>
2021-05-25 09:13:41 +0200

    apply mlr.styler

--- data-raw/ilpd.R
+++ data-raw/ilpd.R
@@ -2,7 +2,7 @@ library("mlr3oml")
 
 odata = OMLData$new(1480)
 ilpd = odata$data
-data.table::setnames(ilpd, c("age", "gender", "total_bilirubin", "direct_bilirubin", "alkaline_phosphatase", 
+data.table::setnames(ilpd, c("age", "gender", "total_bilirubin", "direct_bilirubin", "alkaline_phosphatase",
   "alanine_transaminase", "aspartate_transaminase", "total_protein", "albumin", "albumin_globulin_ratio", "diseased"
 ))
 levels(ilpd$diseased) = c("yes", "no")
