6e7f7ce3269c928bd60f4179badd541c8f74e488
be-marc <marcbecker@posteo.de>
2023-06-27 23:47:09 +0200

    feat: add ames data set

---
+++ data-raw/ames_housing.R
@@ -0,0 +1,3 @@
+url = "https://raw.githubusercontent.com/ja-thomas/extend_ames_housing/main/data/ames_dirty.csv"
+ames_housing = data.table::fread(url, stringsAsFactors = TRUE)
+usethis::use_data(ames_housing, overwrite = TRUE)
